ef32838a1e4d54305730236dd98d622618e6387b
kyokagong <kk1999@kyokagongs-MacBook-Pro-3.local>
2025-09-08 16:25:34 +0800

    update train

--- enova/train/finetune.py
+++ enova/train/finetune.py
@@ -185,7 +185,7 @@ def setup_train_config(dataset_id_list: List[str], eval_dataset_id_list: List[st
                 "overwrite_output_dir": True,
                 ### eval
                 "per_device_eval_batch_size": 1,
-                "predict_with_generate": True,
+                # "predict_with_generate": True,
                 "do_predict": True,
             }
         )
