efecc35b5971f5846be8b64122ba744ab0024035
kyokagong <kk1999@kyokagongs-MacBook-Pro-3.local>
2025-09-10 23:48:42 +0800

    update train

--- enova/train/finetune.py
+++ enova/train/finetune.py
@@ -302,23 +302,23 @@ def modify_chat_templat(model_path, system_prompt):
             + """
 {% endset %}
 {%- if tools %}
-    {{- '<|im_start|>system\n' }}
+    {{- '<|im_start|>system\\n' }}
     {%- if messages[0].role == 'system' %}
-        {{- messages[0].content + '\n\n' }}
+        {{- messages[0].content + '\\n\\n' }}
     {%- else %}
-        {{- default_system_prompt + '\n\n' }}
+        {{- default_system_prompt + '\\n\\n' }}
     {%- endif %}
-    {{- "# Tools\n\nYou may call one or more functions to assist with the user query.\n\nYou are provided with function signatures within <tools></tools> XML tags:\n<tools>" }}
+    {{- "# Tools\\n\\nYou may call one or more functions to assist with the user query.\\n\\nYou are provided with function signatures within <tools></tools> XML tags:\\n<tools>" }}
     {%- for tool in tools %}
-        {{- "\n" }}
+        {{- "\\n" }}
         {{- tool | tojson }}
     {%- endfor %}
-    {{- "\n</tools>\n\nFor each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\n<tool_call>\n{\"name\": <function-name>, \"arguments\": <args-json-object>}\n</tool_call><|im_end|>\n" }}
+    {{- "\\n</tools>\\n\\nFor each function call, return a json object with function name and arguments within <tool_call></tool_call> XML tags:\\n<tool_call>\\n{\"name\": <function-name>, \"arguments\": <args-json-object>}\\n</tool_call><|im_end|>\\n" }}
 {%- else %}
     {%- if messages[0].role == 'system' %}
-        {{- '<|im_start|>system\n' + messages[0].content + '<|im_end|>\n' }}
+        {{- '<|im_start|>system\\n' + messages[0].content + '<|im_end|>\\n' }}
     {%- else %}
-        {{- '<|im_start|>system\n' + default_system_prompt + '<|im_end|>\n' }}
+        {{- '<|im_start|>system\\n' + default_system_prompt + '<|im_end|>\\n' }}
     {%- endif %}
 {%- endif %}
 {%- set ns = namespace(multi_step_tool=true, last_query_index=messages|length - 1) %}
@@ -336,35 +336,35 @@ def modify_chat_templat(model_path, system_prompt):
         {%- set content = '' %}
     {%- endif %}
     {%- if (message.role == "user") or (message.role == "system" and not loop.first) %}
-        {{- '<|im_start|>' + message.role + '\n' + content + '<|im_end|>' + '\n' }}
+        {{- '<|im_start|>' + message.role + '\\n' + content + '<|im_end|>' + '\\n' }}
     {%- elif message.role == "assistant" %}
         {%- set reasoning_content = '' %}
         {%- if message.reasoning_content is string %}
             {%- set reasoning_content = message.reasoning_content %}
         {%- else %}
             {%- if '</think>' in content %}
-                {%- set reasoning_content = content.split('</think>')[0].rstrip('\n').split('<think>')[-1].lstrip('\n') %}
-                {%- set content = content.split('</think>')[-1].lstrip('\n') %}
+                {%- set reasoning_content = content.split('</think>')[0].rstrip('\\n').split('<think>')[-1].lstrip('\\n') %}
+                {%- set content = content.split('</think>')[-1].lstrip('\\n') %}
             {%- endif %}
         {%- endif %}
         {%- if loop.index0 > ns.last_query_index %}
             {%- if loop.last or (not loop.last and reasoning_content) %}
-                {{- '<|im_start|>' + message.role + '\n<think>\n' + reasoning_content.strip('\n') + '\n</think>\n\n' + content.lstrip('\n') }}
+                {{- '<|im_start|>' + message.role + '\\n<think>\\n' + reasoning_content.strip('\\n') + '\\n</think>\\n\\n' + content.lstrip('\\n') }}
             {%- else %}
-                {{- '<|im_start|>' + message.role + '\n' + content }}
+                {{- '<|im_start|>' + message.role + '\\n' + content }}
             {%- endif %}
         {%- else %}
-            {{- '<|im_start|>' + message.role + '\n' + content }}
+            {{- '<|im_start|>' + message.role + '\\n' + content }}
         {%- endif %}
         {%- if message.tool_calls %}
             {%- for tool_call in message.tool_calls %}
                 {%- if (loop.first and content) or (not loop.first) %}
-                    {{- '\n' }}
+                    {{- '\\n' }}
                 {%- endif %}
                 {%- if tool_call.function %}
                     {%- set tool_call = tool_call.function %}
                 {%- endif %}
-                {{- '<tool_call>\n{"name": "' }}
+                {{- '<tool_call>\\n{"name": "' }}
                 {{- tool_call.name }}
                 {{- '", "arguments": ' }}
                 {%- if tool_call.arguments is string %}
@@ -372,26 +372,26 @@ def modify_chat_templat(model_path, system_prompt):
                 {%- else %}
                     {{- tool_call.arguments | tojson }}
                 {%- endif %}
-                {{- '}\n</tool_call>' }}
+                {{- '}\\n</tool_call>' }}
             {%- endfor %}
         {%- endif %}
-        {{- '<|im_end|>\n' }}
+        {{- '<|im_end|>\\n' }}
     {%- elif message.role == "tool" %}
         {%- if loop.first or (messages[loop.index0 - 1].role != "tool") %}
             {{- '<|im_start|>user' }}
         {%- endif %}
-        {{- '\n<tool_response>\n' }}
+        {{- '\\n<tool_response>\\n' }}
         {{- content }}
-        {{- '\n</tool_response>' }}
+        {{- '\\n</tool_response>' }}
         {%- if loop.last or (messages[loop.index0 + 1].role != "tool") %}
-            {{- '<|im_end|>\n' }}
+            {{- '<|im_end|>\\n' }}
         {%- endif %}
     {%- endif %}
 {%- endfor %}
 {%- if add_generation_prompt %}
-    {{- '<|im_start|>assistant\n' }}
+    {{- '<|im_start|>assistant\\n' }}
     {%- if enable_thinking is defined and enable_thinking is false %}
-        {{- '<think>\n\n</think>\n\n' }}
+        {{- '<think>\\n\\n</think>\\n\\n' }}
     {%- endif %}
 {%- endif %}
         """
